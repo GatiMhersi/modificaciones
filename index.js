dd0cef5f6124ecbd7ec04c07e945fed371bc29f2
GatiMhersi <56447063+GatiMhersi@users.noreply.github.com>
2023-07-11 20:37:33 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ var fileRuta = 'index.txt';
 archivoTXT.open("GET", fileRuta, false);
 archivoTXT.send(null);
 var txt = archivoTXT.responseText;
-dato.innerText = txt.split(",")[1];
+for(var i = 0; i<dato.length; i++){
+    dato[i].innerText = txt.split(",")[1];
+}
