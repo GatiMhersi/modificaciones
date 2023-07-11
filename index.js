ad2004233633e0dd391a509e85a4063c192b9c67
GatiMhersi <56447063+GatiMhersi@users.noreply.github.com>
2023-07-11 20:15:42 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var dato = document.getElementById("dato");
+var archivoTXT = new XMLHttpRequest();
+var fileRuta = 'index.txt';
+
+archivoTXT.open("GET", fileRuta, false);
+archivoTXT.send(null);
+var txt = archivoTXT.responseText;
+dato.innerText = txt;
